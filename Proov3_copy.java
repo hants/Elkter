3bc65781de5754b6a6918923eec0ee64c0595754
hannor <hannor@greeny.cs.tlu.ee>
2015-03-27 10:51:25 +0200

    Proov4

---
+++ Proov3_copy.java
@@ -0,0 +1,18 @@
+
+public class Proov3{
+  public static void main(String[] arg){
+    JadaYhendus j=new JadaYhendus();
+    j.lisa(new Takisti220oomi());
+    j.lisa(new Takisti220oomi());
+    System.out.println(j.kysiTakistus());
+    JadaYhendus j2=new JadaYhendus();
+    j2.lisa(new Takisti(1000));
+    j2.lisa(new Takisti(1000));
+    System.out.println(j2.kysiTakistus());
+    JadaYhendus j3=new JadaYhendus();
+    j3.lisa(j);
+    j3.lisa(j2);
+    System.out.println(j3.kysiTakistus());
+              
+  }
+}
